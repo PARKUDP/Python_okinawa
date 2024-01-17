338ceee6f1fa7ac4433c4af5ae5b8bf25d79bfa8
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-01-17 19:29:14 +0900

    filter_data

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -7,19 +7,20 @@ import json
 # 南城市文化財一覧：928764f8-3c8d-4ad1-a269-9547bc788549 ->  取りたいデータは、名称、住所、緯度、経度である
 # 南城市医療機関一覧：24885c01-3fa5-4cbf-b9bf-0cf8d34b3683 ->  取りたいデータは、名称、住所、緯度、経度である
 
-def filter_data(result_table, filter_entry):
+def filter_data(result_table, data, filter_entry):
     filter_input = filter_entry.get().lower()
-    for row in result_table.get_children():
-        if filter_input in result_table.item(row, 'values')[0].lower():
-            result_table.item(row, tags=('visible',))
-        else:
-            result_table.item(row, tags=('hidden',))
-    result_table.tag_configure('visible', foreground='black')
-    result_table.tag_configure('hidden', foreground='white')
+    # Treeview内のデータをクリア
+    for i in result_table.get_children():
+        result_table.delete(i)
+    # フィルタリングされたデータをTreeviewに追加
+    for recode in data:
+        if filter_input in recode['名称'].lower():
+            result_table.insert("", "end", values=(recode['名称'], recode['住所'], recode['緯度'], recode['経度']))
 
 def search_data(search_entry, root):
     # ユーザーの入力を取得
     user_input = search_entry.get()
+    
     if(user_input == "AED"):
         resource = '0106f818-dd7f-4150-bb5e-4207243d8b52'
         
@@ -32,6 +33,7 @@ def search_data(search_entry, root):
     else:
         # 他の場合を追加する場合は、ここに追加する
         pass
+    
     top = tk.Toplevel(root)
     top.title(f"{user_input}の検索結果")
     
@@ -45,9 +47,10 @@ def search_data(search_entry, root):
     filter_entry = tk.Entry(filter_frame)
     filter_entry.pack(side=tk.LEFT)
 
-    filter_button = ttk.Button(filter_frame, text="フィルタ適用", command=lambda: filter_data(result_table, filter_entry))
+    # フィルタリング用のボタンにフィルタ関数を設定
+    filter_button = ttk.Button(filter_frame, text="フィルタ適用", command=lambda: filter_data(result_table, data, filter_entry))
     filter_button.pack(side=tk.LEFT)
-    
+
     result_table = ttk.Treeview(top, columns=("名称", "住所", "緯度", "経度"), show="headings")
     result_table.heading("名称", text="名称")
     result_table.heading("住所", text="住所")
